4031f36d8f43fb1412428cf4d94bebbadc764c4a
Kate <katerebekah@gmail.com>
2015-06-17 15:24:07 -0500

    added xml functionality to handler

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
         xml = d;
       }).success(function() {
         //invoke annotation function
-        annotation();
+        annotation(xml, text);
       })
       .fail(function(err) {
         //add error handling here
@@ -21,26 +21,28 @@
     console.log(err);
   });
 
-  //parent function to handle processing raw materials and adding annotations
-  var annotation = function() {
-    //format xml into an array for ease of use
-    var formatXMLArr = function(xml) {
-      return $('span', xml);
-    }
 
+  var annotation = function(xml, text) {
+    //use xmlHandler library to make new instance of xmlDocument
+    var xmlDoc = new xmlDocument(xml);
+    //format xml into an array for ease of use
+    var xmlArr = xmlDoc.getXMLNodeArr();
     //add annotations - keep link between xml and text with an ID
-    var addAnnotations = function(xmlArr) {
+
+    formatText(addAnnotations(xmlArr));
+
+    function addAnnotations(xmlArr) {
+    var annotatedText = text;
       //start loop at the end 
       //adds formatting text without messing up the start/end count of the rest of the annotations
-      var annotatedText = text;
       for (var i = xmlArr.length - 1; i > -1; i--) {
 
         //find category for class name
-        var category = findCategory(xmlArr[i]);
+        var category = xmlDoc.getNodeCategory(xmlArr[i]);
 
         //find start, end counts to slice text
-        var start = findCount(xmlArr[i], "START");
-        var end = findCount(xmlArr[i], "END") + 1;
+        var start = xmlDoc.getNodeCharPosition(xmlArr[i], "START");
+        var end = xmlDoc.getNodeCharPosition(xmlArr[i], "END") + 1;
 
         //adds necessary formatting necessary to style and interact with text
         var insertText = "<span class='" + category.toLowerCase() + "'>" + annotatedText.slice(start, end) + "<div class='tooltip '>" + category + "<button id='i" + i + "'>Remove</button></div></span>"
@@ -51,31 +53,9 @@
       return annotatedText
     }
 
-    //format text to retain line breaks, append to DOM
-    var formatText = function(text) {
+    function formatText(text) {
       text.replace(/\n/g, '<br>');
       $('.text').append(text);
-    }
-
-    var updateArr = function(index) {
-      xmlArr.splice(index, 1);
-      // removeText();
-      // var replacementText = addAnnotations(xmlArr);
-      // formatText(replacementText);
-      //show updated XML
-      //update counters
-    };
-
-    //*******HELPER FUNCTIONS Not to be Returned!!
-
-    //get names of categories in xml
-    function findCategory(ann) {
-      return ann.getAttribute('category');
-    }
-
-    //finds start and end count for annotations
-    function findCount(ann, str) {
-      return parseInt(ann.lastElementChild.lastElementChild.getAttribute(str));
     }
 
     function removeText() {
@@ -83,30 +63,19 @@
       return;
     }
 
-    //*******END of Helper Functions!!
-
-
-    //does this when invoked
-    var xmlArr = formatXMLArr(xml);
-    var annotatedText = addAnnotations(xmlArr);
-    formatText(annotatedText);
-
-    //return functionalities
-    return {
-      formatXMLArr: formatXMLArr,
-      addAnnotations: addAnnotations,
-      updateArr: updateArr,
-      formatText: formatText
-    }
-  }
 
   //click handler to remove annotation
   //because buttons are added after inital page load, have to do this on .text
   $('.text').on('click', '.tooltip button', function() {
     var id = this.id.toString().slice(1, this.id.length);
-    annotation.updateArr(parseInt(id));
+    xmlDoc.removeNode(parseInt(id));
+    removeText();
+    annotatedText = text;
+    xmlArr = xmlDoc.getXMLNodeArr()
+    formatText(addAnnotations(xmlArr));
   });
 
+  }
   //add function to keep track of how many annotations are in the text
   //save button refresh view and export xml -- push changes into an array?
 //});
---
+++ js/xmlHandler.js
@@ -0,0 +1,16 @@
+function xmlDocument(xml){
+	this.xml = xml;
+	this.getXMLNodeArr = function(){
+      return $('span', xml);
+	};
+	this.getNodeCategory = function(node) {
+     return node.getAttribute('category');
+  };
+  this.getNodeCharPosition = function(node, str) {
+    return parseInt(node.lastElementChild.lastElementChild.getAttribute(str));
+  };
+  this.removeNode = function(index){
+  	this.xml.getElementsByTagName('document')[0].remove(this.xml.getElementsByTagName('document')[0].getElementsByTagName('span')[index]);
+  	return;
+  }
+}
