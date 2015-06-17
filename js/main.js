d9a564618f0fc06b351f3519660703d6780006bd
Kate <katerebekah@gmail.com>
2015-06-17 12:03:21 -0500

    made annotation function invoke after success from ajax

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ $(document).ready(function() {
   $.get('/sources/ch08.txt', function(d) {
     text = d;
   }).success(function() {
-    
+
     $.get('/sources/ch08.txt.xml', function(d) {
-      xml = d;
-    }).success(function() {
-      //invoke annotation function
-    })
-    .fail(function(err) {
-      //add error handling here
-      console.log(err);
-    });
+        xml = d;
+      }).success(function() {
+        //invoke annotation function
+        annotation();
+      })
+      .fail(function(err) {
+        //add error handling here
+        console.log(err);
+      });
 
   }).fail(function(err) {
     //add error handling here
@@ -32,17 +33,17 @@ $(document).ready(function() {
       //start loop at the end 
       //adds formatting text without messing up the start/end count of the rest of the annotations
       for (var i = xmlArr.length - 1; i > -1; i--) {
-        
+
         //find category for class name
         var category = findCategory(xmlArr[i]);
-        
+
         //find start, end counts to slice text
         var start = findCount(xmlArr[i], "START");
         var end = findCount(xmlArr[i], "END") + 1;
-        
+
         //adds necessary formatting necessary to style and interact with text
         var insertText = "<span class='" + category.toLowerCase() + "'>" + text.slice(start, end) + "<div class='tooltip '>" + category + "<button id='i" + i + "'>Remove</button></div></span>"
-        
+
         //adds formatting, updates string so it can be appended to DOM
         text = text.substring(0, start) + insertText + text.substring((start + (end - start)), text.length + end + insertText.length);
       }
@@ -51,7 +52,6 @@ $(document).ready(function() {
 
     //format text to retain line breaks, append to DOM
     var formatText = function(text) {
-      console.log(text);
       text.replace(/\n/g, '<br>');
       $('.text').append(text);
     }
@@ -64,20 +64,27 @@ $(document).ready(function() {
     }
 
     //finds start and end count for annotations
-    function findCount(ann, str){
+    function findCount(ann, str) {
       return parseInt(ann.lastElementChild.lastElementChild.getAttribute(str));
     }
 
     //*******END of Helper Functions!!
 
+
+    //does this when invoked
+    var xmlArr = formatXMLArr(xml);
+    var annotatedText = addAnnotations(xmlArr);
+    formatText(annotatedText);
+
     //return functionalities
     return {
       formatXMLArr: formatXMLArr,
+      addAnnotations: addAnnotations,
       formatText: formatText
     }
   }
 
 
   //add click handler to remove annotation -- remember the on function will be running after text is added dynamically!
-  //save button refresh view and export xml --push changes into an array?
+  //save button refresh view and export xml -- push changes into an array?
 });
