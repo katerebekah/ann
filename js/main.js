be700d409c08cccaf2991bc03cf668fb544a23b6
Kate <katerebekah@gmail.com>
2015-06-17 09:48:10 -0500

    added comments for planning

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
-
+  //retrieve text AJAX
+  //retrieve xml AJAX
+  //format xml - link xml to text 
+  //add annotations - keep link between xml and text - part of span's ID?
+  //format text to retain line breaks - replace \n with <br>
+  //add click handler to remove annotation -- remember the on function will be running after text is added dynamically!
+  //save button refresh view and export xml --push changes into an array?
 });
