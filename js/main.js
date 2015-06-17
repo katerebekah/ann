6dd8f414f1571aaeacc64f64b742cb1151029ace
Kate <katerebekah@gmail.com>
2015-06-17 16:58:04 -0500

    prepared for edit button/function

--- js/main.js
+++ js/main.js
@@ -24,19 +24,23 @@ $(document).ready(function() {
     var xmlDoc = new XmlDocument(xml);
     //format xml into an array for ease of use
     var xmlArr = xmlDoc.getXMLNodeArr();
-
     //prepare div for appending annotated text
     $('.text').empty();
-
     //annotate text and append it to DOM
     $('.text').append(addAnnotationsToString(xmlArr, xmlDoc));
 
     //click handler to remove annotation
     //because buttons are added after inital page load, have to do this on .text
-    $('.text').on('click', '.tooltip button', function() {
-      var id = this.id.toString().slice(1, this.id.length);
+    $('.text').on('click', '.removeButton', function() {
+      var id = $(this).parent().attr('id').toString().slice(1, $(this).parent().attr('id').length);
       xml = xmlDoc.removeNode(parseInt(id));
       annotate(xml, text);
+    });
+    //click handler to edit annotation
+    $('.text').on('click', '.editButton', function(){
+      var id = $(this).parent().attr('id').toString().slice(1, $(this).parent().attr('id').length);
+      
+
     });
 
     //add function to keep track of how many annotations are in the text
@@ -53,7 +57,7 @@ $(document).ready(function() {
       var start = xmlDoc.getNodeCharPosition(xmlArr[i], 'START');
       var end = xmlDoc.getNodeCharPosition(xmlArr[i], 'END') + 1;
       //adds necessary formatting necessary to style and interact with text
-      var insertText = "<span class='" + category.toLowerCase() + "'>" + annotatedText.slice(start, end) + "<div class='tooltip '>" + category + "<button id='i" + i + "'>Remove</button></div></span>";
+      var insertText = "<span class='" + category.toLowerCase() + "'>" + annotatedText.slice(start, end) + "<div class='tooltip' id='i" + i + "'>" + category + "<button class='removeButton'>Remove</button></div></span>";
       //adds formatting, updates string so it can be appended to DOM
       annotatedText = annotatedText.substring(0, start) + insertText + annotatedText.substring((start + (end - start)), annotatedText.length + end + insertText.length);
     }
--- js/xmlHandler.js
+++ js/xmlHandler.js
@@ -1,16 +1,18 @@
-function XmlDocument(xml){
-	this.xml = xml;
-	this.getXMLNodeArr = function(){
-      return $('span', xml);
-	};
-	this.getNodeCategory = function(node) {
-     return node.getAttribute('category');
+'use strict';
+
+function XmlDocument(xml) {
+  this.xml = xml;
+  this.getXMLNodeArr = function() {
+    return $('span', xml);
+  };
+  this.getNodeCategory = function(node) {
+    return node.getAttribute('category');
   };
   this.getNodeCharPosition = function(node, str) {
     return parseInt(node.lastElementChild.lastElementChild.getAttribute(str));
   };
-  this.removeNode = function(index){
-  	this.xml.getElementsByTagName('document')[0].getElementsByTagName('span')[index].remove();
-  	return this.xml;
-  }
+  this.removeNode = function(index) {
+    this.xml.getElementsByTagName('document')[0].getElementsByTagName('span')[index].remove();
+    return this.xml;
+  };
 }
