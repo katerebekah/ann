13c9d852bf07d9cf84b2b367bcf8c84cd445b171
Kate <katerebekah@gmail.com>
2015-06-17 10:34:15 -0500

    added xml to arr

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   var text, xml;
   //retrieve text and XML with AJAX
-  $.get('/sources/ch08.txt', function(d){
-  	text = d;
-  }).success(function(){
-  	$.get('/sources/ch08.txt.xml', function(d){
-  		xml = d;
-  	}).fail(function(err){
-  		//add error handling here
-  		console.log(err);
-  	});
-  }).fail(function(err){
-  	//add error handling here
-  	console.log(err);
+  $.get('/sources/ch08.txt', function(d) {
+    text = d;
+  }).success(function() {
+    $.get('/sources/ch08.txt.xml', function(d) {
+      xml = d;
+    }).fail(function(err) {
+      //add error handling here
+      console.log(err);
+    });
+  }).fail(function(err) {
+    //add error handling here
+    console.log(err);
   });
-  //format xml - link xml to text 
+  var annotation = (function(text, xml) {
+    //format xml into an array for ease of use
+    var formatXMLArr = function(xml) {
+      console.log($('span', xml))
+      return $('span', xml);
+    }
+
+    //return functionalities
+    return {
+      formatXMLArr: formatXMLArr,
+    }
+  })();
+
   //add annotations - keep link between xml and text - part of span's ID?
   //format text to retain line breaks - replace \n with <br>
   //add click handler to remove annotation -- remember the on function will be running after text is added dynamically!
