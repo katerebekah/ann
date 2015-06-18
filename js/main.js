44cedf100cac5c2e9b0b457033dad215d1c459e5
Kate <katerebekah@gmail.com>
2015-06-17 23:52:53 -0500

    polishing, added err handling

--- js/main.js
+++ js/main.js
@@ -6,78 +6,96 @@ $(document).ready(function() {
   $.get('/sources/ch08.txt')
     .success(function(d) {
       text = d;
-      $.get('/sources/ch08.txt.xml', 'xml')
-        .success(function(d) {
-          xml = d;
-          //call annotate
-          annotate(xml, text);
-        }).fail(function(err) {
-          //add error handling here
-          console.log(err);
-        });
-    }).fail(function(err) {
-      //add error handling here
+      getXML()
+    })
+    .fail(function(err) {
       console.log(err);
     });
 
+  function getXML() {
+    $.get('/sources/ch08.txt.xml', 'xml')
+      .success(function(d) {
+        xml = d;
+        //call annotate
+        annotate(xml, text);
+      }).fail(function(err) {
+        console.log(err);
+      });
+  }
+
   function annotate(xml, text) {
-    //use xmlHandler library to make new instance of xmlDocument
     var xmlDoc = new XmlDocument(xml);
+
     //format xml into an array for ease of use
     var xmlArr = xmlDoc.getXMLNodeArr();
-    //prepare div for appending annotated text
-    $('.text').empty();
-    //annotate text and append it to DOM
-    $('.text').append(addAnnotationsToString(xmlArr, xmlDoc));
+
+
+    //Add Elements to DOM
+
+    //empty div & append new annotated text
+    $('.text').empty().append(addAnnotationsToString(xmlArr, xmlDoc));
 
     //add category count to html
-    var categories = xmlDoc.getCategories(xmlArr);
+    var categories = xmlDoc.getCategories();
     for (var i = 0; i < categories.length; i++) {
-      var count = xmlDoc.getCategoryCount(xmlArr, categories[i]);
+      var count = xmlDoc.getCategoryCount(categories[i]);
       $('#' + categories[i]).html(categories[i] + ": " + count);
     }
 
-    //because buttons are added after inital page load, have to do this on .text
+
+    //CLICK HANDLERS
+
+    //remove button
     $('.text').on('click', '.removeButton', function(e) {
+      
       //prevents multiple firings on click
       $('.text').unbind();
+
       var id = $(this).parent().attr('id').toString().slice(1, $(this).parent().attr('id').length);
       xml = xmlDoc.removeNode(parseInt(id));
       annotate(xml, text);
     });
 
-    //click handler to edit annotation
+    //edit an annotation
     $('.text').on('change', '.edit', function(e) {
-      var newCat = e.target.selectedOptions[0].value;
       $('.text').unbind();
-      //prevents multiple firings on click
+      var newCat = e.target.selectedOptions[0].value;
       var id = $(this).parent().attr('id').toString().slice(1, $(this).parent().attr('id').length);
       xml = xmlDoc.editNode(parseInt(id), newCat);
       annotate(xml, text);
     });
+
+    //Handles Tooltips
     $('span').mouseover(function(e) {
       $(this).children()[0].style.display = "block";
     });
+
     $('span').mouseleave(function(e) {
       $(this).children()[0].style.display = "none";
     });
 
   };
 
+
+  //need to pass in xmlDoc with dependency injection
   function addAnnotationsToString(xmlArr, xmlDoc) {
     var annotatedText = text;
+
     //start loop at the end 
     //adds formatting text without messing up the start/end count of the rest of the annotations
     for (var i = xmlArr.length - 1; i > -1; i--) {
+
       //find category for class name
-      var category = xmlDoc.getNodeCategory(xmlArr[i]);
+      var category = xmlDoc.getNodeCategory(i);
+
       //find start, end counts to slice text
-      var start = xmlDoc.getNodeCharPosition(xmlArr[i], 'START');
-      var end = xmlDoc.getNodeCharPosition(xmlArr[i], 'END') + 1;
+      var start = xmlDoc.getNodeCharPosition(i, 'START');
+      var end = xmlDoc.getNodeCharPosition(i, 'END') + 1;
+
       var editButton = "<br><label>Edit Category:</label><select class='edit'><option>Select An Option</option><option value='PERSON'>PERSON</option><option value='LOCATION'>LOCATION</option><option value='ORGANIZATION'>ORGANIZATION</option></select>";
       var deleteButton = "<button class='removeButton'>Remove</button>";
-      //adds necessary formatting necessary to style and interact with text
       var insertText = "<span class='" + category.toLowerCase() + "'>" + annotatedText.slice(start, end) + "<div class='tooltip' id='i" + i + "'><h4>Category: " + category + "</h4>" + editButton + deleteButton + "</div></span>";
+
       //adds formatting, updates string so it can be appended to DOM
       annotatedText = annotatedText.substring(0, start) + insertText + annotatedText.substring((start + (end - start)), annotatedText.length + end + insertText.length);
     }
--- js/xmlHandler.js
+++ js/xmlHandler.js
@@ -2,36 +2,61 @@
 
 function XmlDocument(xml) {
   this.xml = xml;
+  
+  this.parsedXML = this.xml.getElementsByTagName('document')[0];
+  
   this.getXMLNodeArr = function() {
     return $('span', xml);
   };
-  this.getNodeCategory = function(node) {
+  
+  this.getNodeCategory = function(index) {
+  	console.log(typeof(index), index)
+  	if (typeof(index) !== 'number') throw new Error('Must be a number');
+  	var node = this.parsedXML.getElementsByTagName('span')[index];
+  	if (node === null) throw new Error('Node Not Found')
     return node.getAttribute('category');
   };
-  this.getNodeCharPosition = function(node, str) {
-    return parseInt(node.lastElementChild.lastElementChild.getAttribute(str));
+  
+  this.getNodeCharPosition = function(index, str) {
+  	if (typeof(index) !== 'number') throw new Error('Must be a number');
+  	var node = this.parsedXML.getElementsByTagName('span')[index];
+  	if (node === null) throw new Error('Node Not Found')
+  	return parseInt(node.lastElementChild.lastElementChild.getAttribute(str));
   };
+  
   this.removeNode = function(index) {
-    this.xml.getElementsByTagName('document')[0].getElementsByTagName('span')[index].remove();
+  	if (typeof(index) !== 'number') throw new Error('Must be a number');
+  	var node = this.parsedXML.getElementsByTagName('span')[index];
+  	if (node === null) throw new Error('Node Not Found')
+		node.remove();
     return this.xml;
   };
+  
   this.editNode = function(index, newCat) {
-    this.xml.getElementsByTagName('document')[0].getElementsByTagName('span')[index].setAttribute('category', newCat);
+  	if (typeof(index) !== 'number') throw new Error('Must be a number');
+  	if (typeof(newCat) !== 'string') throw new Error('Attribute names must be a string')
+  	var node = this.parsedXML.getElementsByTagName('span')[index];
+  	if (node === null) throw new Error('Node Not Found')
+    node.setAttribute('category', newCat);
     return this.xml;
   };
-  this.getCategories = function(nodeArr){
+  
+  this.getCategories = function(){
   	var categories = [];
+  	var nodeArr = this.getXMLNodeArr();
   	for (var i = 0; i < nodeArr.length; i++){
-  		if (categories.indexOf(this.getNodeCategory(nodeArr[i])) === -1) {
-  			categories.push(this.getNodeCategory(nodeArr[i]));
+  		if (categories.indexOf(this.getNodeCategory(i)) === -1) {
+  			categories.push(this.getNodeCategory(i));
   		}
   	}
   	return categories;
   }
-  this.getCategoryCount = function(nodeArr, category){
+  
+  this.getCategoryCount = function(category){
   	var counter = 0
+  	var nodeArr = this.getXMLNodeArr();
   	for (var i = 0; i < nodeArr.length; i++) {
-  		if (this.getNodeCategory(nodeArr[i]) === category){
+  		if (this.getNodeCategory(i) === category){
   			counter++;
   		}
   	}
